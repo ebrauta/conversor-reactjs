592acefe0f43a5752e200a618677c425034eed66
Eduardo Rauta <ebrauta@gmail.com>
2020-05-09 23:49:03 -0300

    Versão Inicial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root')
+
+const Index = () => {
+    return <h1>Index</h1>
+}
+
+ReactDOM.render(
+    <Index />,
+    rootElement
+);
+
